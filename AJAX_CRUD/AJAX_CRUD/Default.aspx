c5f8ddf11bd6c1a6f320a9565fa9b1ad389fdcef
Yuvraj <yuvrajsoni9192@gmail.com>
2024-03-29 09:42:15 +0530

    First

---
+++ AJAX_CRUD/AJAX_CRUD/Default.aspx
@@ -0,0 +1,87 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="AJAX_CRUD.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+<td>Name</td>
+<td><input type="text" id="t1" /></td>
+                </tr>
+                
+                   <tr>
+<td>city</td>
+<td><input type="text" id="t2" /></td>
+                </tr>
+
+                                   <tr>
+
+<td><input type="button" id="b1" value="insert" onclick="insert();" /></td>
+<td><input type="button" id="b2" value="display" onclick="display();" /></td>
+<td><input type="button" id="btn3" value="Delete" onclick="delete1();" /></td>   
+          <td><input type="button" id="btn3" value="Update" onclick="update1();" /></td>                             
+                </tr>
+
+            </table>
+            <div id ="d1">
+
+            </div>
+        </div>
+    </form>
+
+    <script type="text/javascript">
+        function insert() {
+            var name = document.getElementById("t1").value;
+            var city = document.getElementById("t2").value;
+
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.open("GET", "insert.aspx?nm=" + name + "&ct=" + city + "&opr=insert", false);
+            xmlhttp.send(null);
+
+            var name = document.getElementById("t1").value = "";
+            var city = document.getElementById("t2").value = "";
+            alert('Recored Inserted');
+        }
+        function display() {
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.open("GET", "insert.aspx?opr=display", false)
+            xmlhttp.send(null);
+
+            document.getElementById("d1").innerHTML = xmlhttp.responseText;
+        }
+        function delete1() {
+            var name = document.getElementById("t1").value;
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.open("GET", "insert.aspx?nm=" + name + "&opr=delete1", false);
+            xmlhttp.send(null);
+
+            var name = document.getElementById("t1").value = "";
+            alert('DELETE');
+
+        }
+        function update1() {
+            var oldname = document.getElementById("t1").value;
+            var newname = document.getElementById("t2").value;
+
+
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.open("GET", "insert.aspx?oldname=" + oldname + "&newname=" + newname + "&opr=update1", false);
+            xmlhttp.send(null);
+
+            document.getElementById("t1").value = "";
+            document.getElementById("t2").value = "";
+
+
+            alert('Update');
+
+        }
+
+    </script>
+</body>
+</html>
---
+++ AJAX_CRUD/AJAX_CRUD/insert.aspx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net;
+using System.Drawing;
+
+namespace AJAX_CRUD
+{
+    public partial class insert : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Admin\Documents\test.mdf;Integrated Security=True;Connect Timeout=30;");
+
+
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string name, city, opr , oldname, newname;
+            opr = Request.QueryString["opr"].ToString();
+            
+
+            if (opr == "insert")
+            {
+                name = Request.QueryString["nm"].ToString();
+                city = Request.QueryString["ct"].ToString();
+
+                conn.Open();
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "insert into Test_1 values('" + name.ToString() + "','" + city.ToString() + "')";
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            if (opr == "display")
+            {
+
+                conn.Open();
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "select * from Test_1";
+                cmd.ExecuteNonQuery();
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+
+                Response.Write("<table>");
+
+                Response.Write("<tr>");
+                Response.Write("<td>"); Response.Write("name"); Response.Write("</td>");
+                Response.Write("<td>"); Response.Write("city"); Response.Write("</td>");
+                Response.Write("</tr>");
+
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    Response.Write("<tr>");
+                    Response.Write("<td>"); Response.Write(dr["name"].ToString()); Response.Write("</td>");
+                    Response.Write("<td>"); Response.Write(dr["city"].ToString()); Response.Write("</td>");
+                    Response.Write("</tr>");
+
+                }
+
+            }
+            if (opr == "delete1")
+            {
+                name = Request.QueryString["nm"].ToString();
+                conn.Open();
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "delete from Test_1 where name='" + name.ToString() + "' ";
+                cmd.ExecuteNonQuery();
+
+
+                conn.Close();
+            }
+            if (opr == "update1")
+            {
+                oldname = Request.QueryString["oldname"].ToString();
+                newname = Request.QueryString["newname"].ToString();
+
+                conn.Open();
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "update Test_1 set name='" + newname.ToString() + "' where name='" + oldname.ToString() + "'";
+                cmd.ExecuteNonQuery();
+
+
+                conn.Close();
+            }
+
+        }
+    }
+}
